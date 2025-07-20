157ff903181de8de0c15d44afc51f07cd36ff17d
Tariq Omar <tariqomar2004@gmail.com>
2025-07-20 18:17:18 +0300

    implement profiles for models in presentation layer

---
+++ Src/TABP.WebAPI/MappingProfiles/RoomAmenityMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TABP.Domain.Entities;
+using TABP.Domain.QueryFilters.EntitiesFilters;
+using TABP.WebAPI.Models.RoomAmenity;
+
+namespace TABP.WebAPI.MappingProfiles;
+
+public class RoomAmenityMappingProfile : Profile
+{
+    public RoomAmenityMappingProfile()
+    {
+        CreateMap<RoomAmenityRequestDto, RoomAmenity>();
+        CreateMap<RoomAmenityFilterDto, RoomAmenityFilter>();
+        CreateMap<RoomAmenity, RoomAmenityResponseDto>();
+    }
+}

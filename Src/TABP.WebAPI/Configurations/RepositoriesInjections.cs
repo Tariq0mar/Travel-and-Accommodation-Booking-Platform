5f0c80a30467ef6922ec2c2cbd000537973c4966
Tariq Omar <tariqomar2004@gmail.com>
2025-07-29 20:35:57 +0300

    register dependencies

---
+++ Src/TABP.WebAPI/Configurations/RepositoriesInjections.cs
@@ -0,0 +1,33 @@
+﻿using TABP.Domain.Interfaces.Repositories;
+using TABP.Infrastructure.Repositories;
+
+namespace TABP.WebAPI.Configurations;
+
+public static class RepositoriesInjections
+{
+    public static IServiceCollection AddAppRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IAmenityRepository, AmenityRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        services.AddScoped<ICartItemRepository, CartItemRepository>();
+        services.AddScoped<IDiscountRepository, DiscountRepository>();
+        services.AddScoped<IGalleryRepository, GalleryRepository>();
+        services.AddScoped<IHotelRepository, HotelRepository>();
+        services.AddScoped<IHotelAmenityRepository, HotelAmenityRepository>();
+        services.AddScoped<IHotelDiscountRepository, HotelDiscountRepository>();
+        services.AddScoped<IHotelGalleryRepository, HotelGalleryRepository>();
+        services.AddScoped<ILocationRepository, LocationRepository>();
+        services.AddScoped<IPaymentRepository, PaymentRepository>();
+        services.AddScoped<IReviewRepository, ReviewRepository>();
+        services.AddScoped<IRoomRepository, RoomRepository>();
+        services.AddScoped<IRoomAmenityRepository, RoomAmenityRepository>();
+        services.AddScoped<IRoomCategoryRepository, RoomCategoryRepository>();
+        services.AddScoped<IRoomCategoryDiscountRepository, RoomCategoryDiscountRepository>();
+        services.AddScoped<IRoomGalleryRepository, RoomGalleryRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserDiscountRepository, UserDiscountRepository>();
+
+        return services;
+    }
+}
+
--- Src/TABP.WebAPI/Program.cs
+++ Src/TABP.WebAPI/Program.cs
@@ -1,11 +1,47 @@
+using System.Text;
 using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using TABP.WebAPI.Configurations;
 using TABP.WebAPI.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Register your repositories
+builder.Services.AddAppRepositories();
+
+// Register services
+builder.Services.AddAppServices();
+
+// Register AutoMapper
+builder.Services.AddAutoMapper(typeof(Program));
+
+// Register Controller
 builder.Services.AddControllers();
 
-//Register Rate Limiter
+// Authentication
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "yourissuer",
+            ValidAudience = "youraudience",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("your_secret_key_here"))
+        };
+    });
+
+// Authorization 
+builder.Services.AddAuthorization();
+
 builder.Services.AddRateLimiter(options =>
 {
     options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
